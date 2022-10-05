a7ea62561e0fa633662789f254eec00fc8f507ce
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-10-05 09:45:50 +0800

    add Skeleton loading in ProductList page

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import { Row, Col, Spin } from "antd";
+import { Row, Col, Spin, Skeleton } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
 import { useSelector } from "react-redux";
 import { selectIsLoading } from "../redux/productsSlice";
@@ -11,27 +11,21 @@ export default function ProductList({ products }) {
   const isLoading = useSelector(selectIsLoading);
 
   return (
-    <>
-      {isLoading ? (
-        <div className="spinner-wrap">
-          <Spin indicator={antIcon} className="spinner" />
-        </div>
-      ) : (
-        <Row gutter={[32, 32]}>
-          {products.map(product => (
-            <Col
-              key={product.id}
-              sm={{ span: 12 }}
-              lg={{ span: 8 }}
-              xl={{ span: 6 }}
-              xxl={{ span: 4 }}
-            >
-              <ProductItem product={product} />
-            </Col>
-          ))}
-        </Row>
-      )}
-    </>
+    <Skeketon loading={isLoading}>
+      <Row gutter={[32, 32]}>
+        {products.map(product => (
+          <Col
+            key={product.id}
+            sm={{ span: 12 }}
+            lg={{ span: 8 }}
+            xl={{ span: 6 }}
+            xxl={{ span: 4 }}
+          >
+            <ProductItem product={product} />
+          </Col>
+        ))}
+      </Row>
+    </Skeketon>
   );
 }
 
