87579f47cf9dee713a4c552a590bb73fa5e93699
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-11-22 11:05:35 +0800

    change to railwayapp server

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const URL = "http://127.0.0.1:5000/api/v1"
+const URL = "https://fastapi4railway2022-production.up.railway.app/api/v1"
 
 export const getProductById = async ({ queryKey }) => {
   const [productId] = queryKey;
